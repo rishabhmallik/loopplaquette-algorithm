de9a8e95c2950b1aa01d4cc7e8a6d12196010931
rishabhmallik <87011291+rishabhmallik@users.noreply.github.com>
2021-07-11 17:57:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -327,7 +327,7 @@ int main(int argc, char **argv)
         //phi4_avg=phi4_avg/configs;
         
         data<<mu<<"\t"<<n_avg<<"\t"<<errorjack(xi);
-        data1<<"\t"<<phi2_avg<<"\t"<<errorjack(phi2i);
+        data1<<mu<<"\t"<<phi2_avg<<"\t"<<errorjack(phi2i);
         
         mu+=dmu;
         cout<<g<<endl;
